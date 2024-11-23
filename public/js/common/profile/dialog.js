2bd843a0d0bd1de48ca892aef841546db462c5b5
Dreamer-Guy <CoolGuyLearning35@gmail.com>
2024-11-24 06:31:33 +0700

    fix dialog

---
+++ public/js/common/profile/dialog.js
@@ -0,0 +1,7 @@
+const handleCloseDialog=(id)=>{
+    document.getElementById(id).classList.add('hidden');
+}
+
+const handleOpenDialog=(id)=>{
+    document.getElementById(id).classList.remove('hidden');
+};
