66a48d10f1425a617cd080abe8ef33cbfc6390fa
Duc Toan <22120376@student.hcmus.edu.vn>
2024-11-23 21:50:48 +0700

    implement update address

--- src/Address/dbService.js
+++ src/Address/dbService.js
@@ -1,7 +1,19 @@
-
+import Address from "../Model/Address.js";
 
 const addressService={
-    
+    getAddressByUserId: async (id) => {
+        return Address.findOne({userId:id}).lean();
+    },
+    updateAddress: async (id, address) => {
+        return Address.updateOne({userId:id},address);
+    },
+    saveAddress: async(address)=>{
+        await address.save();
+        return address;
+    },
+    getAllAddresses: async () => {
+        return Address.find().lean();
+    },
 };
 
 
