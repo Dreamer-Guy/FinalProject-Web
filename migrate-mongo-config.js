2fec54ee9f5cde814aff85d4fdb10cb0c1b584c3
Le Thanh Vinh <thanhvinh2400@gmail.com>
2024-12-03 22:34:34 +0700

    mock new database

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -1,7 +1,7 @@
 const config = {
   mongodb: {
-    url: "mongodb://admin:admin@localhost:27023",
-    databaseName: "",
+    url: "mongodb+srv://vinh01515:YrQQXix0hXIPZLD2@cluster0.c6dwy.mongodb.net",
+    databaseName: "doanDB",
     options: {
       // connectTimeoutMS: 3600000, // Optional: increase connection timeout
       // socketTimeoutMS: 3600000,  // Optional: increase socket timeout
