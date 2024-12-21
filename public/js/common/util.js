111cbf676928072deea539fec2cdc2683e59f586
Dreamer-Guy <CoolGuyLearning35@gmail.com>
2024-12-21 22:54:12 +0700

    add keypress event for search bar

--- public/js/common/util.js
+++ public/js/common/util.js
@@ -2,3 +2,10 @@ function handleSearch(){
     const value=document.getElementById('search').value;
     window.location.href=`/products/get?search=${encodeURIComponent(value)}`;
 }
+
+
+document.getElementById('search').addEventListener('keypress',function(e){
+    if(e.key==='Enter'){
+        handleSearch();
+    }
+});
